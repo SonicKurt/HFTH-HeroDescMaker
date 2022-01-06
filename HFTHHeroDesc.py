e2045f5d240c32e8acc4d2c447093b102c637455
Kurt Campbell <44346740+SonicKurt@users.noreply.github.com>
2022-01-06 15:35:57 -0500

    added method for mission count

--- HFTHHeroDesc.py
+++ HFTHHeroDesc.py
@@ -11,6 +11,7 @@ Date: 20 November 2021
 
 Modifications:
     2022-01-04 - Added condition statements within holiday_filler method
+    2022-01-06 - Implemented method for how the mission should be printed
 
 © Copyright Holidays For The Heroes, Kurt Campbell.
 All rights reserved.
@@ -55,16 +56,31 @@ def holiday_filler(holiday_name):
     elif holiday_name == "":
         return ""
     else:
-        return f" for{holiday_name}"
+        return f" for {holiday_name}"
+
+def mission_filler(mission_num):
+    remainder = int(mission_num) % 10
+    if remainder == 1:
+        return f"{mission_num}st"
+    elif remainder == 2:
+        return f"{mission_num}nd"
+    elif remainder == 3:
+        return f"{mission_num}rd"
+    else:
+        return f"{mission_num}th"
+
 
 results_file = open("results.txt", "a")
 another_hero = "Y"
+mission_count = 0
+mission_str = ""
 
 print("Holidays For The Heroes Hero Description Maker")
 
 choice = pick_option()
 
 while choice != "3":
+    mission_count = 0
     if choice == "1":
         try:
             filename = input("Enter the file name of your csv: ")
@@ -86,42 +102,52 @@ while choice != "3":
                 holiday_name = hero[5]
                 holiday_name = holiday_filler(holiday_name)
 
-                hero_desc = f"Meet {hero_name}! {gender} is an {branch}{based_name} and went home{location}{holiday_name}!\n"
+                mission_count += 1
+                mission_str = mission_filler(mission_count)
+
+                hero_desc = f"Meet {hero_name}! {gender} was our {mission_str} mission. {gender} is an {branch}{based_name} and went home{location}{holiday_name}!\n"
                 results_file.write(hero_desc)
                 print(hero_desc.strip())
-            print(f"\nGenerated all hero desciptions from {filename} and stored in results.txt")
+            print(f"\nGenerated hero desciptions given the information from {filename}.\nThey are stored in results.txt")
             choice = pick_option()
         except FileNotFoundError:
             print(f"{filename} is not found.")
             choice = pick_option()
     elif choice == "2":
-        while (another_hero == "Y"):
+        while another_hero == "Y":
             print("Please enter the hero's name, branch, location")
             # Essential inputs for a hero description.
-            hero_name = input("Name: ").capitalize()
+            hero_name = input("Name: ")
 
             gender = input("Gender (Enter 1-Male or 2-Female): ").capitalize()
             if gender == "1":
                 gender = "He"
             else:
                 gender = "She"
+            
+            mission_count = input("Mission Number: ")
+            while not mission_count.isdigit():
+                print("You entered a non-numerical value. Please enter a number.")
+                mission_count = input("Mission Number: ")
+            
+            mission_str = mission_filler(mission_count)
 
             branch = input("Branch (1-Army, 2-Air Force, 3-Navy," + 
                 "4-Coast Guard, 5-Marine): ").capitalize()
 
             branch = branch_filler(branch)
 
-            location = input("Location: ").capitalize()
+            location = input("Location: ")
             location = location_filler(location)
 
-            based_name = input("Based: ").capitalize()
+            based_name = input("Based: ")
             based_name = based_filler(based_name)
 
             holiday_name = input("Holidays: ")
             holiday_name = holiday_filler(holiday_name)
-                
-            hero_desc = f"Meet {hero_name}! {gender} is an {branch}{based_name} and went home{location} for {holiday_name}!\n"
-
+            
+            hero_desc = f"Meet {hero_name}! {gender} was our {mission_str} mission. {gender} is an {branch}{based_name} and went home{location}{holiday_name}!\n"
+                  
             print(hero_desc)
 
             results_file.write(hero_desc)
